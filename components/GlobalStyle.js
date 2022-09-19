fc1c8a5b53cf1e8e46a04ece7456cae3466f6591
DoroVernier <doro.vernier@outlook.de>
2022-09-19 15:15:39 +0200

    add global-style component

---
+++ components/GlobalStyle.js
@@ -0,0 +1,13 @@
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *, *::after, *::before {
+        box-sizing: border-box
+    }
+    body {
+        background-color: #eee
+    }
+`;
+
+export default GlobalStyle;
