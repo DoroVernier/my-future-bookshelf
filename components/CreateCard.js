3be66947918c59dda86333a1a42a678ffd3a7e81
DoroVernier <doro.vernier@outlook.de>
2022-09-24 17:29:21 +0200

    add counter for both input-fields

--- components/CreateCard.js
+++ components/CreateCard.js
@@ -1,7 +1,18 @@
 import { nanoid } from 'nanoid';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 export default function CreateCard({ onAddBook }) {
+  const [titleCount, setTitleCount] = useState(0);
+  const titleCounter = (event) => {
+    setTitleCount(event.target.value.length);
+  };
+
+  const [authorCount, setAuthorCount] = useState(0);
+  const authorCounter = (event) => {
+    setAuthorCount(event.target.value.length);
+  };
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -19,27 +30,31 @@ export default function CreateCard({ onAddBook }) {
   return (
     <form onSubmit={handleSubmit}>
       <Note htmlFor="title">
-        Note the title:
+        Note title, author or ISBN:
         <NoteField
           type="text"
           name="title"
           id="title"
-          placeholder="Title (input required)"
+          placeholder="(required)..."
           maxLength="70"
+          onChange={titleCounter}
           required
         />
+        <Counter>{titleCount}/70</Counter>
       </Note>
       <br />
       <Note htmlFor="author">
-        Note the author:
+        Further info:
         <NoteField
           type="text"
           name="author"
           id="author"
-          placeholder="Author"
+          placeholder="(optional)..."
           maxLength="70"
+          onChange={authorCounter}
         />
       </Note>
+      <Counter>{authorCount}/70</Counter>
       <WishButton>Wish</WishButton>
     </form>
   );
@@ -58,4 +73,9 @@ const Note = styled.label`
 `;
 const NoteField = styled.input`
   margin-left: 1rem;
+`;
+
+const Counter = styled.span`
+  margin-left: 0.5rem;
+  font-size: 0.5rem;
 `;
