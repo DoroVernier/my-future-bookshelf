9ad294bfbe4177d5c967482b0b28363b7b12e017
DoroVernier <doro.vernier@outlook.de>
2022-10-04 12:48:32 +0200

    max-input, add-button

--- components/CreateCard.js
+++ components/CreateCard.js
@@ -1,7 +1,8 @@
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import styled from 'styled-components';
-
+import Image from 'next/image';
+import add from '../public/add.png';
 const apiURL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'; // ISBN adden
 // const apiURL = 'https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}'; // ISBN adden
 
@@ -14,14 +15,19 @@ export default function CreateCard({ onAddBook }) {
     setTitleCount(event.target.value.length);
   };
 
-  const [authorCount, setAuthorCount] = useState(0);
-  const authorCounter = (event) => {
-    setAuthorCount(event.target.value.length);
-  };
+  // const [authorCount, setAuthorCount] = useState(0);
+  // const authorCounter = (event) => {
+  //   setAuthorCount(event.target.value.length);
+  // };
 
-  const resetCount = () => {
-    setTitleCount(initialCount);
-    setAuthorCount(initialCount);
+  // const resetCount = () => {
+  //   setTitleCount(initialCount);
+  //   setAuthorCount(initialCount);
+  // };
+
+  const [inputCount, setInputCount] = useState(0);
+  const inputCounter = (event) => {
+    setInputCoung(event.target.value.length);
   };
 
   function handleSubmit(event) {
@@ -65,8 +71,15 @@ export default function CreateCard({ onAddBook }) {
   return (
     <Form onSubmit={handleSubmit}>
       <Note htmlFor="isbn">
-        Title, author or ISBN:
-        <NoteField type="text" name="isbn" id="isbn" required />
+        ISBN:
+        <NoteField
+          type="text"
+          name="isbn"
+          id="isbn"
+          maxLength="13"
+          placeholder="9780571200832"
+          required
+        />
       </Note>
 
       {/* <Note htmlFor="title">
@@ -95,7 +108,11 @@ export default function CreateCard({ onAddBook }) {
         />
         <Counter>{authorCount}/70</Counter>
       </Note> */}
-      <WishButton onClick={resetCount}>Wish</WishButton>
+      <WishButton
+      // onClick={resetCount}
+      >
+        <Image alt="add" layout="responsive" src={add} width={64} height={64} />
+      </WishButton>
     </Form>
   );
 }
@@ -129,7 +146,8 @@ const Counter = styled.span`
 
 const WishButton = styled.button`
   grid-area: 1 2 3 3;
-  background-color: red;
-  border: 1px solid black;
+  background-color: transparent;
+  height: 3rem;
+  width: 3rem;
   border-radius: 100%;
 `;
