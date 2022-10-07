554d592b5647b5a784ab3b31fabe8ff636766e35
DoroVernier <doro.vernier@outlook.de>
2022-10-07 20:17:22 +0200

    implement local storage

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,18 @@ import booksForStyle from './../public/body-background_4.jpg';
 import BookCard from '../components/BookCard';
 import CreateCard from '../components/CreateCard';
 import styled from 'styled-components';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+function loadFromLocal() {
+  try {
+    return JSON.parse(localStorage.getItem('book'));
+  } catch (error) {
+    return false;
+  }
+}
 
 export default function WishList() {
-  const [bookList, setBookList] = useState([]);
+  const [bookList, setBookList] = useLocalStorage('book', []);
 
   function addBook(savedBook) {
     setBookList([savedBook, ...bookList]);
